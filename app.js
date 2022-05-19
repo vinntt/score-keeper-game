6176f012e85f2b6e3649c8b06e6c9ab0d70be411
vinntt <vinh.ngtt@gmail.com>
2022-05-19 23:20:56 +0200

    refactored reset button function

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ let isGameOver = false;
 
 function updateScore(player, opponent) {
     if (!isGameOver) {
-        player.score +=1;
-        if (player.score  === winningScore) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameOver = true
             player.display.classList.add("has-text-success");
             opponent.display.classList.add("has-text-danger");
@@ -29,15 +29,15 @@ function updateScore(player, opponent) {
     }
 }
 
-p1.button.addEventListener("click", function() {
+p1.button.addEventListener("click", function () {
     updateScore(p1, p2)
 })
 
-p2.button.addEventListener("click", function() {
+p2.button.addEventListener("click", function () {
     updateScore(p2, p1)
 })
 
-winningScoreSelect. addEventListener("change", function() {
+winningScoreSelect.addEventListener("change", function () {
     winningScore = parseInt(this.value);
     reset();
 })
@@ -46,12 +46,18 @@ resetButton.addEventListener("click", reset)
 
 function reset() {
     isGameOver = false;
-    p1.score = 0;
-    p2.score = 0;
-    p1.display.textContent = 0;
-    p2.display.textContent = 0;
-    p1.display.classList.remove("has-text-success", "has-text-danger");
-    p2.display.classList.remove("has-text-success", "has-text-danger");
-    p1.button.disabled = false;
-    p2.button.disabled = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove("has-text-success", "has-text-danger");
+        p.button.disabled = false;
+    }
+    // p1.score = 0;
+    // p2.score = 0;
+    // p1.display.textContent = 0;
+    // p2.display.textContent = 0;
+    // p1.display.classList.remove("has-text-success", "has-text-danger");
+    // p2.display.classList.remove("has-text-success", "has-text-danger");
+    // p1.button.disabled = false;
+    // p2.button.disabled = false;
 }
